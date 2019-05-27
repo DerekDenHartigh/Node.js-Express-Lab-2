5d574b869386863866a39818b493979c04a74da9
Derek <derekdenhartigh@gmail.com>
2019-05-26 20:52:58 -0400

    first commit, express lab 2

---
+++ cart-items.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let cartItems = [
+    {"id": 1, "product": "banana", "price": 15, "quantity": 99},
+    {"id": 2, "product": "cofee-beans", "price": 10, "quantity": 1},
+    {"id": 3, "product": "dvd", "price": 5, "quantity": 4},
+    {"id": 4, "product": "underpants", "price": 3, "quantity": 6},
+    {"id": 5, "product": "brocoli", "price": 32, "quantity": 2},
+    {"id": 6, "product": "shoe", "price": 45, "quantity": 1},
+    {"id": 7, "product": "eye-patch", "price": 8, "quantity": 2},
+];
+
+module.exports = cartItems;
---
+++ server.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const express = require('express');
+const pg = require('pg');
+const cartItemsPage = require("./cartItemsPage.js");
+const app = express();
+app.use(express.json());
+
+app.use("/",cartItemsPage);
+app.use(express.static("./public")); // allows access to public directory which has front-end business
+const port = 3000;
+// runs the server
+app.listen(port, ()=> console.log(`listening on port: http://localhost:${port}`));
